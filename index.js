94461d65ed4566aebe4b1ecb17eb67999a0c0957
KyleHS <k15156@yahoo.com>
2023-07-10 12:56:54 -0600

    sent a response with res.send

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.use(() => {
     console.log("New request")
-})
+    res.send("We received the request. This is the response")
+})  //responds every time index.js is called in node
 
 //to find in browser go to:   localhost:2000
 app.listen(2000, () => {  //sets up server locally on machine.  Port is 2000
