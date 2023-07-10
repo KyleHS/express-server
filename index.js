ec3751be421c29c950cb14f3f43ff31c30ef329a
KyleHS <k15156@yahoo.com>
2023-07-10 13:03:53 -0600

    added more get requests in express to establish different pages in server.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
-    console.log("New request")
-    res.send("We received the request. This is the response")
-})  //responds every time index.js is called in node
+//app.use(() => {
+ //   console.log("New request")
+ //   res.send("We received the request. This is the response")  //can only be done once
+//})  //responds every time index.js is called in node
+
+app.get('/', (req, res) => {  //method is known as routing
+    res.send("homepage")
+})
+
+app.get('/cats', (req, res) => {  //makes it so that we have to go to localhost:2000/cats  to get this request
+    res.send("Cat Request")
+})
+
+app.get('/dogs', (req, res) => {
+    res.send("Dog Request")
+})
 
 //to find in browser go to:   localhost:2000
 app.listen(2000, () => {  //sets up server locally on machine.  Port is 2000
